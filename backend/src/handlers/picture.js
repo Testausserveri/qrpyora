8bcc89ea0ea209aaf294f393f1e6a55456d376ea
Roni Äikäs <roni.aikas@nordcloud.com>
2022-06-24 01:23:18 +0300

    Add blurring API

--- backend/src/handlers/picture.js
+++ backend/src/handlers/picture.js
@@ -7,6 +7,8 @@ const {discordWebHook} = require('../webhook/client')
 const instagramClient = require('../instagram/client');
 const reFormatters = require("../utils/reformatters");
 const fs = require('fs');
+const axios = require('axios');
+const FormData = require('form-data');
 async function triggerHook(bike, url, timestamp) {
     await discordWebHook(bike, url, timestamp);
 }
@@ -31,10 +33,18 @@ async function upload(req, res, db, hook=true) {
                 return;
             }
             const { buffer } = req.file;
+            const formData = new FormData();
+            formData.append('qrcode', buffer, { contentType: 'image/jpeg', filename: "qrpyora.jpg" });
             const fileName = uuid.v4() + '.jpg';
+            const uploadResponse = await axios.post(process.env.BLUR_URI || 'http://blur:3000/blur', formData, {
+              headers: {
+                'Content-Type': `multipart/form-data; boundary=${formData._boundary}`
+              },
+              responseType: 'arraybuffer'
+            });
             const picture = await db.addPicture(fileName, req.params.bikeId);
             const metadata = await sharp(buffer).metadata();
-            let sharpImg = sharp(buffer)
+            let sharpImg = sharp(Buffer.from(uploadResponse.data, 'binary'))
             if (metadata.width > 1500 || metadata.height > 1500) {
                 sharpImg = sharpImg.resize(metadata.width > metadata.height ? 1500 : undefined, metadata.height > metadata.width ? 1500 : undefined);
             }
